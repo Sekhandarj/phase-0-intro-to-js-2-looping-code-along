42ff4e76a0875b07c7c7d80adc12d328234dc389
Sekhandar Jagdeo <Sekhandarj@gmail.com>
2023-07-09 19:41:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = surprise
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+
+  
+  function countDown(i) {
+    console.log(i);
+    i>0 && countDown(i-1);
+}
+    countDown(10);
